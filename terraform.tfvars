cf145d87ef12ec55d03d82f6d2cc463b3fa8625b
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-01-15 17:09:47 +0000

    MEOW-1: add a jenkins_allowed_ip variable

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,4 @@
 aws_region = "eu-west-1"
+
+jenkins_allowed_ip = "86.11.152.40"
+
--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "aws_region" {
   type    = string
-  description = "AWS region where we will create the infrastructure"
+  description = "AWS region where we will create the infrastructure" 
+}
+
+variable "jenkins_allowed_ip" {
+  type = string
+  description = "IP Address which is allowed to access the Jenkins instance"
 }
