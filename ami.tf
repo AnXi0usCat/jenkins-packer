e6c3dca5361f13c4263cd6b857d2c8f2fb5caee9
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2023-01-15 21:40:41 +0000

    MEOW-1: add ubuntu ami instance

---
+++ ami.tf
@@ -0,0 +1,30 @@
+resource aws_ami "ubuntu-jenkins" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "jenkins-instance" {
+  ami           = data.aws_ami.ubuntu-jenkins.id
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.jenkins-security-group.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.jenkins-keys.key_name
+}
